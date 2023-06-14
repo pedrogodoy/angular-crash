c743e7137bc92820fc7089f02dc44ecf4f0f2409
Pedro <pedrogh.godoy12@gmail.com>
2023-06-14 17:22:17 -0300

    feat: change mock to api call

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3001/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
