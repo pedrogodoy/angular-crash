bef9f497a6f5b2a8f9f885095d64cbf07602a799
Pedro <pedrogh.godoy12@gmail.com>
2023-06-14 14:37:03 -0300

    feat: create mock task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appoitment',
+    day: '5 de maio às 14:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Doctors Appoitment',
+    day: '5 de maio às 14:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Doctors Appoitment',
+    day: '5 de maio às 14:00',
+    reminder: true,
+  },
+]
