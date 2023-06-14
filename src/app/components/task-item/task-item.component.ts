57625cf45c6bbb29dbc985bb12ff05f30a92b6d8
Pedro <pedrogh.godoy12@gmail.com>
2023-06-14 18:03:32 -0300

    feat: toggle reminder - WIP

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -10,11 +10,17 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent {
   @Input() task: Task = { text: '', day: '', reminder: true }
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
 
   faTimes = faTimes;
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 
 }
