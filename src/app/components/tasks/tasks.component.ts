ed90d31d8f52382bd70bc4afa262b594abb34463
Pedro <pedrogh.godoy12@gmail.com>
2023-06-14 15:30:44 -0300

    feat: create task item and observable example

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private tasksService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasksService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+}
